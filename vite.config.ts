c155bc46fdb6c74a13892dbe65abe9f4561adae9
Mika Steyer <mikasteyer9@gmail.com>
2025-11-08 15:52:53 +0100

    chore: configure proper import alias support

---
+++ vite.config.ts
@@ -0,0 +1,12 @@
+import path from "node:path";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src/"),
+    },
+  },
+});
+
+console.log("qwe");
